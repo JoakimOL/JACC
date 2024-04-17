45ee1ac7433a0ac8762ec769f160e84f7aa4090c
JoakimOL <joakim.o.l@hotmail.com>
2024-04-17 12:44:15 +0200

    Add --first flag
    This flags stops the pipeline after generating the first set.
    Could be useful for educational purposes, to inspect the sets

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,15 @@ int main(int argc, char *argv[])
     argparse::ArgumentParser program(argv[0]);
     program.add_argument("-f");
     program.add_argument("-v").default_value(false).implicit_value(true);
+    program.add_argument("--first").default_value(false).implicit_value(true);
     program.parse_args(argc, argv);
     if (program.is_used("-v")) {
         spdlog::set_level(spdlog::level::debug);
+    }
+
+    bool generate_first_sets_only = false;
+    if (program.is_used("--first")) {
+        generate_first_sets_only = true;
     }
     auto filename = program.present("-f");
     spdlog::info("filename: {}", filename.has_value() ? *filename : "nullopt");
@@ -25,4 +31,5 @@ int main(int argc, char *argv[])
 
     Grammar::set_map<ProductionSymbol> first_sets = driver.grammar.generate_first_sets();
     spdlog::info("first sets: {}", first_sets);
+    if(generate_first_sets_only) return 0;
 }
